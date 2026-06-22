778f4493bb7137956694ca4e197cf3c4363a6734
alphy <evjacheslav@gmail.com>
2026-06-22 15:13:57 +0300

    fetchpoints instruction

--- src/main/java/net/voiddustry/redvsblue/logic/LInstructions.java
+++ src/main/java/net/voiddustry/redvsblue/logic/LInstructions.java
@@ -2,6 +2,8 @@ package net.voiddustry.redvsblue.logic;
 
 import arc.func.Prov;
 import arc.scene.ui.layout.Table;
+import arc.util.Log;
+import mindustry.game.Team;
 import mindustry.gen.LogicIO;
 import mindustry.gen.Player;
 import mindustry.gen.Unit;
@@ -19,6 +21,9 @@ public final class LInstructions {
     public static void register() {
         LogicIO.allStatements.add((Prov<LStatement>) GivePointsStatement::new);
         LAssembler.customParsers.put(GivePointsStatement.OPCODE, GivePointsStatement::read);
+
+        LogicIO.allStatements.add((Prov<LStatement>) FetchPointsStatement::new);
+        LAssembler.customParsers.put(FetchPointsStatement.OPCODE, FetchPointsStatement::read);
     }
 
     public static final class GivePointsStatement extends LStatement {
@@ -87,6 +92,89 @@ public final class LInstructions {
             if (data == null) return;
 
             data.addScore((int) amount.numval);
+        }
+    }
+
+    public static final class FetchPointsStatement extends LStatement {
+        public static final String OPCODE = "fetchpoints";
+
+        public String unit = "0";
+        public String result = "result";
+
+        @Override
+        public String name() {
+            return OPCODE;
+        }
+
+        @Override
+        public boolean privileged() {
+            return true;
+        }
+
+        @Override
+        public void build(Table table) {
+        }
+
+        @Override
+        public LExecutor.LInstruction build(LAssembler builder) {
+            return new FetchPointsI(builder.var(unit), builder.var(result));
+        }
+
+        @Override
+        public void write(StringBuilder builder) {
+            builder.append(OPCODE).append(' ');
+            builder.append(unit).append(' ');
+            builder.append(result).append(' ');
+        }
+
+        public static LStatement read(String[] tokens) {
+            FetchPointsStatement s = new FetchPointsStatement();
+            if (tokens.length > 1) s.unit = tokens[1];
+            if (tokens.length > 2) s.result = tokens[2];
+            return s;
+        }
+    }
+
+    public static final class FetchPointsI implements LExecutor.LInstruction {
+        public LVar unit;
+        public LVar result;
+
+        public FetchPointsI() {}
+
+        public FetchPointsI(LVar unit, LVar result) {
+            this.unit = unit;
+            this.result = result;
+        }
+
+        @Override
+        public void run(LExecutor exec) {
+            if (unit == null || result == null) {
+                return;
+            }
+
+
+            if (!(unit.objval instanceof Unit u)) {
+                result.setobj(null);
+                return;
+            }
+
+
+            Player player = u.getPlayer();
+
+
+            if (player == null) {
+                result.setobj(null);
+                return;
+            }
+
+            PlayerData data = RedVsBluePlugin.players.get(player.uuid());
+
+            if (data == null) {
+                result.setobj(null);
+                return;
+            }
+
+            result.setnum(data.getScore());
         }
     }
 }
--- worldlogicmod/src/rvsblogicmod/GivePointsStatement.java
+++ worldlogicmod/src/rvsblogicmod/GivePointsStatement.java
@@ -61,11 +61,71 @@ public class GivePointsStatement extends LStatement {
     public static void register() {
         LogicIO.allStatements.add((Prov<LStatement>) GivePointsStatement::new);
         LAssembler.customParsers.put(OPCODE, GivePointsStatement::read);
+
+        LogicIO.allStatements.add((Prov<LStatement>) FetchPointsStatement::new);
+        LAssembler.customParsers.put(FetchPointsStatement.OPCODE, FetchPointsStatement::read);
     }
 
     public static final class GivePointsI implements LExecutor.LInstruction {
         public GivePointsI() {}
         public GivePointsI(LVar unit, LVar amount) {}
+
+        @Override
+        public void run(LExecutor exec) {}
+    }
+
+    public static final class FetchPointsStatement extends LStatement {
+        public static final String OPCODE = "fetchpoints";
+
+        public String unit = "0";
+        public String result = "result";
+
+        @Override
+        public String name() {
+            return OPCODE;
+        }
+
+        @Override
+        public LCategory category() {
+            return LCategory.operation;
+        }
+
+        @Override
+        public boolean privileged() {
+            return true;
+        }
+
+        @Override
+        public void build(Table table) {
+            table.add("unit ");
+            field(table, unit, str -> unit = str).width(110f).padRight(8f);
+            table.add("result ");
+            field(table, result, str -> result = str).width(90f);
+        }
+
+        @Override
+        public LExecutor.LInstruction build(LAssembler builder) {
+            return new FetchPointsI(builder.var(unit), builder.var(result));
+        }
+
+        @Override
+        public void write(StringBuilder builder) {
+            builder.append(OPCODE).append(' ');
+            builder.append(unit).append(' ');
+            builder.append(result).append(' ');
+        }
+
+        public static LStatement read(String[] tokens) {
+            FetchPointsStatement s = new FetchPointsStatement();
+            if (tokens.length > 1) s.unit = tokens[1];
+            if (tokens.length > 2) s.result = tokens[2];
+            return s;
+        }
+    }
+
+    public static final class FetchPointsI implements LExecutor.LInstruction {
+        public FetchPointsI() {}
+        public FetchPointsI(LVar unit, LVar result) {}
 
         @Override
         public void run(LExecutor exec) {}
