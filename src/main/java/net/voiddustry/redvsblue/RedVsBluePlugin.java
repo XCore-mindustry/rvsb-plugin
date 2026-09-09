d67db1d64899136c561f4f5a2873d647059d7939
alphy <evjacheslav@gmail.com>
2026-09-09 20:18:19 +0300

    lol

--- src/main/java/net/voiddustry/redvsblue/RedVsBluePlugin.java
+++ src/main/java/net/voiddustry/redvsblue/RedVsBluePlugin.java
@@ -243,7 +243,7 @@ public class RedVsBluePlugin extends Plugin {
         //blue kill registration
         Events.on(EventType.UnitDestroyEvent.class, event -> {
             
-            if (!(event.unit.hasEffect(Vars.content.statusEffect("noRegisterKills"))) && !(event.unit.type instanceof MissileUnitType)) {
+            if (!(event.unit.hasEffect(Vars.content.statusEffect("dp-no-register-kills"))) && !(event.unit.type instanceof MissileUnitType)) {
                 Player killerPlayer = killCredit.get(event.unit);
                 killCredit.remove(event.unit);
                 
